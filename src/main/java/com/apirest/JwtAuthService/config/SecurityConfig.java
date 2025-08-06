ca7ad784b59e5fc97a82b37b5e0427c51a17ecfc
{{brunoSandoval210}} <{{bruno.sandoval210@gmail.com}}>
2025-08-06 14:10:11 -0500

    mod config cors

--- src/main/java/com/apirest/JwtAuthService/config/SecurityConfig.java
+++ src/main/java/com/apirest/JwtAuthService/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,6 +47,7 @@ public class SecurityConfig {
 //                })
                 .addFilterBefore(jwtTokenValidator(), BasicAuthenticationFilter.class)
                 .addFilterBefore(jwtTokenGenerator(), BasicAuthenticationFilter.class)
+                .cors(Customizer.withDefaults())
                 .build();
     }
 
